2bdb16aeadf36ad4a0309bab05d1eb662620b4f0
Elie <precieuxbatonda@gmail.com>
2023-04-11 00:04:31 +0200

    first part done

--- menu.js
+++ menu.js
@@ -5,7 +5,7 @@ const menu =  [
       title: "buttermilk pancakes",
       category: "breakfast",
       price: 15.99,
-      img: "./images/item-1.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg",
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
     },
     {
@@ -13,7 +13,7 @@ const menu =  [
       title: "diner double",
       category: "lunch",
       price: 13.99,
-      img: "./images/item-2.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg",
       desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
     },
     {
@@ -21,7 +21,7 @@ const menu =  [
       title: "godzilla milkshake",
       category: "shakes",
       price: 6.99,
-      img: "./images/item-3.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg",
       desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
     },
     {
@@ -29,7 +29,7 @@ const menu =  [
       title: "country delight",
       category: "breakfast",
       price: 20.99,
-      img: "./images/item-4.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg",
       desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
     },
     {
@@ -37,7 +37,7 @@ const menu =  [
       title: "egg attack",
       category: "lunch",
       price: 22.99,
-      img: "./images/item-5.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg",
       desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
     },
     {
@@ -45,7 +45,7 @@ const menu =  [
       title: "oreo dream",
       category: "shakes",
       price: 18.99,
-      img: "./images/item-6.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg",
       desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
     },
     {
@@ -53,7 +53,7 @@ const menu =  [
       title: "bacon overflow",
       category: "breakfast",
       price: 8.99,
-      img: "./images/item-7.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-7.jpeg",
       desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
     },
     {
@@ -61,7 +61,7 @@ const menu =  [
       title: "american classic",
       category: "lunch",
       price: 12.99,
-      img: "./images/item-8.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-8.jpeg",
       desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
     },
     {
@@ -69,7 +69,7 @@ const menu =  [
       title: "quarantine buddy",
       category: "shakes",
       price: 16.99,
-      img: "./images/item-9.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-9.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
     {
@@ -77,7 +77,7 @@ const menu =  [
       title: "steak dinner",
       category: "dinner",
       price: 39.99,
-      img: "./images/item-10.jpeg",
+      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-10.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
   ];
@@ -90,6 +90,13 @@ const mealContainers = document.querySelector('.mealContainers');
 //Event Listeners
 window.addEventListener('DOMContentLoaded', function(){
     let displayMenu = menu.map(function(menu){
-        console.log(menu);
-    })
+        return `<section class="menuItems">
+        <img src= ${menu.img} alt="${menu.title}"></img>
+        <h4 id="mealName">${menu.category} <span id="price">${menu.price}</span></h4>
+        <span id="borderBottomMenu"></span>
+        <p>${menu.desc}
+    </section>`
+    });
+    displayMenu = displayMenu.join('');
+    mealContainers.innerHTML = displayMenu
 })
