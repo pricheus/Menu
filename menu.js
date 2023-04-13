4c119fc414714bcc2a233bef03257f3e740cb9dc
Elie <precieuxbatonda@gmail.com>
2023-04-13 22:44:41 +0200

    second time oing the project

--- menu.js
+++ menu.js
@@ -1,127 +1,120 @@
-//list of items
-const menu =  [
-    {
-      id: 1,
-      title: "buttermilk pancakes",
-      category: "breakfast",
-      price: 15.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg",
-      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-    },
-    {
-      id: 2,
-      title: "diner double",
-      category: "lunch",
-      price: 13.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg",
-      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-    },
-    {
-      id: 3,
-      title: "godzilla milkshake",
-      category: "shakes",
-      price: 6.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg",
-      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-    },
-    {
-      id: 4,
-      title: "country delight",
-      category: "breakfast",
-      price: 20.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg",
-      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-    },
-    {
-      id: 5,
-      title: "egg attack",
-      category: "lunch",
-      price: 22.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg",
-      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-    },
-    {
-      id: 6,
-      title: "oreo dream",
-      category: "shakes",
-      price: 18.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg",
-      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-    },
-    {
-      id: 7,
-      title: "bacon overflow",
-      category: "breakfast",
-      price: 8.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-7.jpeg",
-      desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
-    },
-    {
-      id: 8,
-      title: "american classic",
-      category: "lunch",
-      price: 12.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-8.jpeg",
-      desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
-    },
-    {
-      id: 9,
-      title: "quarantine buddy",
-      category: "shakes",
-      price: 16.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-9.jpeg",
-      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-    },
-    {
-      id: 10,
-      title: "steak dinner",
-      category: "dinner",
-      price: 39.99,
-      img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-10.jpeg",
-      desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
-    },
-  ];
+//items list
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, 
+    hammock freegan copper mug whatever
+     cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "https://vanilla-js-basic-project-8-menu.netlify.app/images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
 
 //interactive elements
+const mealContainers = document.querySelector('.mealContainer');
 const buttons = document.querySelectorAll('button');
-const mealContainers = document.querySelector('.mealContainers');
 
 
-//load items
-window.addEventListener('DOMContentLoaded', function(){
-   displayMenuItems(menu)
-});
-//filtering option
-buttons.forEach(function(buttons){
-    buttons.onclick = function(e){
-      const type= e.currentTarget.dataset.id;
-      const category = menu.filter(function(menuItem){
-        if(menuItem.category === type){
-           return menuItem;
-        } 
-      });
 
-      
-      if(type === 'all'){
-        displayMenuItems(menu)
-      } else {
-        displayMenuItems(category)
-      }     
-    }
+//render content dynamically
+window.addEventListener('DOMContentLoaded',function(){
+const tag=  menu.map(displayMenu).join("");
+  mealContainers.innerHTML =tag
 })
 
 
-function displayMenuItems(menuItems){
-     let displayMenu = menuItems.map(function(item){
-       return ` <section class="menuItems">
-       <img src=${item.img} alt="${item.title}"></img>
-       <h4 id="mealName">${item.title} <span id="price">${item.price}</span></h4>
-       <span id="borderBottomMenu"></span>
-       <p>${item.desc}</p>
-   </section>` 
-    });
-    displayMenu = displayMenu.join('');
-    mealContainers.innerHTML = displayMenu;
+function displayMenu(menu){
+  return `<section class="mealItems">
+  <img src=${menu.img} alt=${menu.title}>
+  <aside>
+      <h4>${menu.title} <span id="price">${menu.price}</span></h4>
+      <span id="dotted_border"></span>
+      <p>
+      ${menu.desc}
+      </p>
+  </aside>
+</section>`
 }
+//filtering functionnality
 
+buttons.forEach(function(btn){
+  btn.onclick = function(e){
+    const currentBtnCategory =  e.currentTarget.dataset.id;
+    console.log(currentBtnCategory)
+  }
+})
 
